3c23bc6f74ece2a902c1dd6995690f15de5b05b6
Matt Way <matt__way@hotmail.com>
2017-01-19 23:19:23 +1000

    transpile fix

--- src/types.js
+++ src/types.js
@@ -1,6 +1,6 @@
 /**
  * This types file is used, in case there needs to be multi module type
- * matching at some stage. Grambda converts to the correct types when
+ * matching at some stage. twobyfour converts to the correct types when
  * being parsed by certain modules. It is only for scalar types currently
  */
 import { 
