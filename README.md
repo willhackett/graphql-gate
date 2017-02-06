a84684f2138f3e6509eb87820fe200fdb0be8c88
Matt Way <matt__way@hotmail.com>
2017-02-06 16:26:33 +1000

    readme update

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # twobyfour
 graphql and database agnostic wrapper to help create APIs quickly.
+
+## Required Setup
+
+- youtube api key for meta data on video add
+- aws keys/iam for dynamodb and serverless
--- src/utils.js
+++ src/utils.js
@@ -6,7 +6,7 @@ const mapObj = (o, f, ctx) => {
   ctx = ctx || this
   var result = {}
   Object.keys(o).forEach(k => {
-    result[k] = f.call(ctx, o[k], k, o);
+    result[k] = f.call(ctx, o[k], k, o)
   })
   return result
 }
